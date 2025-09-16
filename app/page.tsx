0779929416cef9d37e4eac78d6f40a9e1c65134e
Koma <josepgb04@gmail.com>
2025-09-16 16:53:36 +0200

    Very Important (AND temporal) changes
    EASTER EGG

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
         <div>
             <p>Home page</p>
 
-            <Link href={"/test-page"}>Go to test page</Link>
+            <p><Link href={"/test-page"}>Go to test page</Link></p>
+
+            <b><Link href={"/funny"}>Go to Josep's epic page</Link></b>
+
         </div>
     );
 }
