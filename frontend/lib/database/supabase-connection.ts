5c593efe33a7fa7e82a8f54d19cf4519e975173d
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-09-23 10:58:57 +0200

    supabase auth implemented

--- frontend/lib/database/supabase-connection.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "./supabase";
-
-const supabaseKey = process.env.SUPABASE_API_KEY || "";
-const supabaseUrl = process.env.SUPABASE_URL || "http://127.0.0.1:8000";
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
