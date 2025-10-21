010186475886d31c295dba5ff2d144fd29da0357
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-10-21 12:53:12 +0200

    some styles fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { H1, H2 } from "@/components/ui/typography";
-import Link from "next/link";
+import { H1 } from "@/components/ui/typography";
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center">
 			<H1>Deal&Buy</H1>
-
-			<Link href={"/home"}>
-				<H2>Go to home</H2>
-			</Link>
 		</section>
 	);
 }
