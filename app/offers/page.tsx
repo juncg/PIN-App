869a6788494c7958f90b8c122df94b3f7cfec818
Koma <josepgb04@gmail.com>
2025-10-22 09:59:53 +0200

    Fixing spaghetti code from ubren
    Changed offers/petitions page.tsx using link instead of router; which needs use client.

--- app/offers/page.tsx
+++ app/offers/page.tsx
@@ -1,18 +1,18 @@
-"use client";   
-
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 
 export default async function Offers() {
-    const router = useRouter();
-
     return (
-		<section className="flex flex-row justify-center gap-8">
-			<div className="flex flex-center flex-col gap-8">
+        <section className="flex flex-row justify-center gap-8">
+            <div className="flex flex-center flex-col gap-8">
                 <h1>Ofertas</h1>
                 <h2>Esto no esta implementado</h2>
-                <Button onClick={() => router.push("/offers/create")}>Crear una Oferta</Button>
+                <Link href="/offers/create">
+                    <Button>Crear una oferta</Button>
+                </Link>
+
+
             </div>
         </section>
     );
--- app/petitions/page.tsx
+++ app/petitions/page.tsx
@@ -2,23 +2,21 @@ import { Button } from "@/components/ui/button";
 import { H1, H2 } from "@/components/ui/typography";
 import { GetFromDatabase } from "@/lib/services/general";
 import { IPetition } from "@/lib/services/types";
-import { useRouter } from "next/navigation";
 import { PostCard } from "@/components/cards/postCard";
 import { PetitionServices } from "./page-services";
+import Link from "next/link";
 
 export default async function Petitions() {
-    const router = useRouter();
     const { petitions } = await PetitionServices();
 
     return (
         <section className="flex flex-row justify-center gap-8">
-            <div className="flex flex-center flex-col gap-8">
-                <H1>Peticiones</H1>
-                <H2>Esto no esta implementado</H2>
-                <Button onClick={() => router.push("/petitions/create")}>Crear una Petición</Button>
-            </div>
+
             <div className="flex flex-col items-baseline gap-8 w-1/2">
                 <H1>Peticiones</H1>
+                <Link href="/petitions/create">
+                    <Button>Crear una petición</Button>
+                </Link>
 
                 {petitions?.map((petition: IPetition) => (
                     <PostCard
