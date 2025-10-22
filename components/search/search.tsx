708622236f79989b9daacfbcfad6d3727897158a
Rubén Guaita <rguadav@upv.edu.es>
2025-10-22 10:47:06 +0200

    small fix

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -27,7 +27,7 @@ export default function SearchItems({ items, postType }: SearchItemsProps) {
       <div className="relative max-w-md">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-muted-foreground" />
         <Input
-          placeholder="Buscar ofertas..."
+          placeholder={`Buscar ${postType === "Oferta" ? "ofertas" : "peticiones"}...`}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           className="pl-10 glass"
