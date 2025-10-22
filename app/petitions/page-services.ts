ab7c573bdfd43643d23c6f7bd24dd50984983495
Koma <josepgb04@gmail.com>
2025-10-22 09:36:26 +0200

    programando
    no se que estoy haciendo ayuda

---
+++ app/petitions/page-services.ts
@@ -0,0 +1,9 @@
+import { GetFromDatabase } from "@/lib/services/general";
+import { IPetition } from "@/lib/services/types";
+
+
+export async function PetitionServices() {
+    const petitions = await GetFromDatabase<IPetition>({ tableName: "Petition", select: "*" });
+
+    return { petitions };
+}
--- app/petitions/page.tsx
+++ app/petitions/page.tsx
@@ -1,12 +1,14 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { H1, H2 } from "@/components/ui/typography";
+import { GetFromDatabase } from "@/lib/services/general";
+import { IPetition } from "@/lib/services/types";
 import { useRouter } from "next/navigation";
+import { PostCard } from "@/components/cards/postCard";
+import { PetitionServices } from "./page-services";
 
-
-export default function Petitions() {
+export default async function Petitions() {
     const router = useRouter();
+    const { petitions } = await PetitionServices();
 
     return (
         <section className="flex flex-row justify-center gap-8">
@@ -15,6 +17,27 @@ export default function Petitions() {
                 <H2>Esto no esta implementado</H2>
                 <Button onClick={() => router.push("/petitions/create")}>Crear una Petición</Button>
             </div>
+            <div className="flex flex-col items-baseline gap-8 w-1/2">
+                <H1>Peticiones</H1>
+
+                {petitions?.map((petition: IPetition) => (
+                    <PostCard
+                        key={petition.id}
+                        props={{
+                            className: "w-full",
+                            businessName: "N/A",
+                            productDescription: petition.text || "N/A",
+                            productName: petition.title || "N/A",
+                            typeOfPost: "Petición",
+                            peopleSignedCurrent: petition.current_progress || 0,
+                            peopleSignedObjective: petition.target_progress || 0,
+                        }}
+                    />
+                ))}
+            </div>
         </section>
+
+
+
     );
 }
