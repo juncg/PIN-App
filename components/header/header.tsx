3a3d6fec43710f265e75c3c29474bce8e7551c3c
juncg <juancarlos.gonzalez.jcgon@gmail.com>
2025-10-22 13:04:28 +0200

    me cago en quien haya cambiado la puta sidebar de los huevos joder

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,20 +1,18 @@
+import Link from "next/link";
 import { AuthButton } from "../auth/auth-buttons";
+import { SidebarTrigger } from "../ui/sidebar";
 import { ThemeSwitcher } from "../ui/theme-switcher";
-import { AppNameLink } from "./app-name-link";
-import { SidebarWrapper } from "./sidebar-wrapper";
 
 export function Header() {
 	return (
 		<header className="sticky top-0 z-30 w-full flex border-b border-b-foreground/10 h-16 bg-background">
-			<div className="w-full flex justify-between items-center px-4 md:px-6 relative">
+			<div className="w-full flex justify-between items-center px-4 md:px-6">
 				<div className="flex items-center gap-2">
 					<SidebarTrigger />
 					<Link className="font-semibold text-lg" href={"/home"}>
 						Deal&Buy
 					</Link>
 				</div>
-
-				<AppNameLink />
 
 				<div className="flex gap-2 md:gap-4 items-center">
 					<AuthButton />
