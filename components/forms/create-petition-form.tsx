d59af856d29070f448b1d5893ca8ab5c5e793eb8
Rubén Guaita <rguadav@upv.edu.es>
2025-11-18 14:29:15 +0100

    mini fix

--- components/forms/create-petition-form.tsx
+++ components/forms/create-petition-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import SelectTags from "@/components/select/select-tags";
+import { SelectTags } from "@/components/select/select-tags";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
