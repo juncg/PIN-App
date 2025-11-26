1d0e2c36ae96e258194da5e51d127ae78c0c7669
Koma <josepgb04@gmail.com>
2025-11-26 20:11:50 +0100

    homepage.spec.ts finished

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,10 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('offers can be created and viewed afterwards', async ({ page }) => {
-    test.use({ storageState: 'storageState.json' });
-    await page.goto('/offers?locale=en');
+test.use({ storageState: 'storageState.json' });
 
-    await page.getByRole('button', { name: 'Nueva Oferta' }).click();
+test('offers can be created and viewed afterwards', async ({ page }) => {
+    
+    await page.goto('/auth/login?locale=es');
+
+    // Fill login form
+    await page.getByPlaceholder('m@example.com').fill('josepgb04@gmail.com');
+    await page.locator('#password').fill('0123456');
+
+
+    // Click login button
+    await page.getByRole('button', { name: 'Iniciar sesión' }).click(); //copiando save-login-state pq las cookkies no quieren ir
+
+    await page.waitForTimeout(2000); // give it time to load stuff
+    await page.goto('/offers?locale=en');
+    
+
+    await page.locator('button:has-text("Nueva Oferta")').click();
+
 
     await page.locator('#title').fill('Oferta test random' + (Math.random() * 100) );
 
@@ -16,7 +31,9 @@ test('offers can be created and viewed afterwards', async ({ page }) => {
 
     await page.locator('#target_completition_date').click(); // abre date picker
 
-    await page.getByRole('button', { name: 'November 28, 2025' }).click(); // hace click en una fecha
+    await page.getByRole('button', { name: 'Saturday, November 29th, 2025' }).click(); // hace click en una fecha 
+    // WARNING: HAY QUE CAMBIAR MANUALMENTE FECHA CADA VEZ, si alguien sabe arreglarlo q lo arregle xd
+    // probablmente se pueda cogiendo la fecha actual y pasandolo a una string con el formato que usamos 
 
     await page.locator('#forum_id').click(); // dropdown foro
 
@@ -26,7 +43,7 @@ test('offers can be created and viewed afterwards', async ({ page }) => {
 
     await page.getByRole('button', { name: 'Crear Oferta' }).click(); // boton crear oferta se clicka al final
 
-
+    
 
 
 });
