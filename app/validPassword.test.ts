01b236dd867979b82af1b8e48665f411d840827a
Koma <josepgb04@gmail.com>
2025-11-27 13:20:16 +0100

    added more tests to validPassword

--- app/validPassword.test.ts
+++ app/validPassword.test.ts
@@ -1,5 +1,21 @@
 import { isPasswordValid } from "./validPassword";
 //ignore errors with test/expect
-test("is this password valid?", () => {
+test("basic password", () => {
   expect(isPasswordValid("hello")).toBe(false);
+});
+
+test("only numbers", () => {
+  expect(isPasswordValid("123123")).toBe(false);
+});
+
+test("only symbols", () => {
+  expect(isPasswordValid("?!?´`")).toBe(false);
+});
+
+test("only uppercase", () => {
+  expect(isPasswordValid("HELLO")).toBe(false);
+});
+
+test("valid password", () => {
+  expect(isPasswordValid("JuanCarlos123!")).toBe(true);
 });
