3dd222db9dc6dc06cd3fb32b473c9c4cbd25adda
Rubén Guaita <rguadav@upv.edu.es>
2025-12-12 19:53:11 +0100

    form de editar perfil arreglado

--- components/ui-custom/textarea.tsx
+++ components/ui-custom/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
 		<textarea
 			data-slot="textarea"
 			className={cn(
-				"border-hover placeholder:text-lightgrey focus-visible:border-lightgrey focus-visible:ring-lightgrey/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-hover/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+				"border-hover placeholder:text-lightgrey focus-visible:border-lightgrey focus-visible:ring-lightgrey/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-hover/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-white text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
 				className
 			)}
 			{...props}
